92456f98541b0fadfe2b253eb7dd68dbfdcf14ec
Centurionactionx <joseph789222@gmail.com>
2025-07-13 21:31:54 -0700

    checkpoitns

--- Dead Roads/Assets/Scripts/PlayerMovement.cs
+++ Dead Roads/Assets/Scripts/PlayerMovement.cs
@@ -4,81 +4,131 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement")]
     public Rigidbody2D playerRb;
     public float speed;
-    public float input;
-    public SpriteRenderer playerSprite;
     public float jumpForce;
+    private float input;
 
+    [Header("Jumping & Ground Detection")]
     public LayerMask groundMask;
-    private bool isGrounded;
     public Transform feetPosition;
-    public float groundCheckCircle;
-
-    public Animator animator;
-    // public Sprite defaultSprite;
-    // public Sprite jumpFlashSprite;
-
+    public float groundCheckRadius = 0.1f;
+    private bool isGrounded;
     private bool jumpRequested = false;
 
-    void Start(){
+    [Header("Animation & Sprites")]
+    public SpriteRenderer playerSprite;
+    public Animator animator;
+
+    [Header("Checkpoints")]
+    public Vector3 defaultStartPosition = new Vector3(0.621f, 0.038f, 0f);
+    public Vector2[] checkpointPositions; // Set in Inspector
+    private bool[] checkpointTriggered;
+    private Vector3 lastCheckpointPosition;
+
+    [Header("Audio")]
+    public AudioClip checkpointSoundClip;
+    private AudioSource audioSource;
+
+    void Start()
+    {
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+
+        lastCheckpointPosition = defaultStartPosition;
+
+        // Initialize checkpoint tracking
+        checkpointTriggered = new bool[checkpointPositions.Length];
     }
 
     void Update()
-{
-    input = Input.GetAxisRaw("Horizontal");
-
-    if (input < 0){
-        playerSprite.flipX = true;
-    } else if (input > 0){
-        playerSprite.flipX = false;
-    }
-
-    // Constantly check grounded status
-    isGrounded = Physics2D.OverlapCircle(feetPosition.position, groundCheckCircle, groundMask);
-
-    // Update animator with jump state
-    animator.SetBool("isJumping", !isGrounded);
-
-    if ((Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.W)) && !jumpRequested)
     {
-        // StartCoroutine(PlayJumpFlash()); // plays sprite briefly
-        StartCoroutine(DelayedJump());
-    }
-
-
-    if (transform.position.y < -30f)
-    {
-        transform.position = new Vector3(0.621f, 0.038f, 0f);
-        playerRb.velocity = Vector2.zero;
-    }
-    }
-
-    // IEnumerator PlayJumpFlash()
-    // {
-    //     playerSprite.sprite = jumpFlashSprite;
-    //     yield return new WaitForSeconds(0.1f); // duration to show jump flash
-    //     playerSprite.sprite = defaultSprite;
-    // }
-
-    IEnumerator DelayedJump()
-    {
-        jumpRequested = true;
-        yield return new WaitForSeconds(0.05f); // jump delay
-
-        if (isGrounded)
-        {
-            playerRb.velocity = Vector2.up * jumpForce;
-        }
-
-        jumpRequested = false; // reset for next jump
+        HandleInput();
+        HandleDirection();
+        HandleGroundCheck();
+        HandleJump();
+        HandleReset();
+        CheckCheckpoints();
     }
 
     void FixedUpdate()
     {
         playerRb.velocity = new Vector2(input * speed, playerRb.velocity.y);
         animator.SetFloat("x_velocity", Mathf.Abs(playerRb.velocity.x));
-        animator.SetFloat("y_velocity", playerRb.velocity.y);    
+        animator.SetFloat("y_velocity", playerRb.velocity.y);
+    }
+
+    void HandleInput()
+    {
+        input = Input.GetAxisRaw("Horizontal");
+    }
+
+    void HandleDirection()
+    {
+        if (input < 0)
+            playerSprite.flipX = true;
+        else if (input > 0)
+            playerSprite.flipX = false;
+    }
+
+    void HandleGroundCheck()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPosition.position, groundCheckRadius, groundMask);
+        animator.SetBool("isJumping", !isGrounded);
+    }
+
+    void HandleJump()
+    {
+        if ((Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.W)) && !jumpRequested)
+        {
+            StartCoroutine(DelayedJump());
+        }
+    }
+
+    void HandleReset()
+    {
+        if (transform.position.y < -80f || Input.GetKeyDown(KeyCode.R))
+        {
+            ResetPosition();
+        }
+    }
+
+    void CheckCheckpoints()
+    {
+        for (int i = 0; i < checkpointPositions.Length; i++)
+        {
+            if (!checkpointTriggered[i] &&
+                transform.position.x >= checkpointPositions[i].x &&
+                Mathf.Abs(transform.position.y - checkpointPositions[i].y) <= 2f)
+            {
+                lastCheckpointPosition = new Vector3(checkpointPositions[i].x, checkpointPositions[i].y, 0f);
+                checkpointTriggered[i] = true;
+
+                if (checkpointSoundClip != null)
+                {
+                    audioSource.PlayOneShot(checkpointSoundClip);
+                }
+            }
+        }
+    }
+
+    IEnumerator DelayedJump()
+    {
+        jumpRequested = true;
+        yield return new WaitForSeconds(0.001f);
+
+        if (isGrounded)
+        {
+            playerRb.velocity = Vector2.up * jumpForce;
+        }
+
+        jumpRequested = false;
+    }
+
+    void ResetPosition()
+    {
+        transform.position = lastCheckpointPosition;
+        playerRb.velocity = Vector2.zero;
     }
 }
